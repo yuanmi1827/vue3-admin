601f7ff82167b11e9dc8c4c180e1546d6facc83c
yuanmi <1366701108@qq.com>
2022-11-16 12:40:45 +0800

    feat: 初始化项目

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ module.exports = {
             {
                 singleQuote: false, // 使用单引号
                 semi: false, // 末尾添加分号
-                // tabWidth: 2,
+                tabWidth: 2,
                 trailingComma: "none",
                 useTabs: false,
                 endOfLine: "auto",
